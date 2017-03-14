c43e4d672dbe28711efa6c1e9785553030b0e777
Andrius <andriusjavait@gmail.com>
2017-03-14 02:05:50 +0000

    SelectTagHandler fully complete class to produce html tag <select> and <option> using attributes like optionList, name and size

--- Chapter10_6/src/com/example/taglib/SelectTagHandler.java
+++ Chapter10_6/src/com/example/taglib/SelectTagHandler.java
@@ -27,7 +27,29 @@ public class SelectTagHandler extends SimpleTagSupport{
 		this.size = size;
 	}
 	
-	public void doTag(){
+	// generate the <select> and <option> tags
+	public void doTag()throws JspException, IOException{
+		PageContext pageContext = (PageContext)getJspContext();
+		JspWriter out = pageContext.getOut();
 		
-	}
-}
+		// start the HTML <select> tag with HTML-specific attributes:
+		// The HTML <select> open tag uses the name and size attributes:
+		out.println("<p>Select beer characteristic</p>");
+		out.println("Color:");
+		out.print("<select");
+		out.print(String.format(ATTR_TEMPLATE, "name",this.name));
+		out.print(String.format(ATTR_TEMPLATE, "size",this.size));
+		out.println(">");
+		
+		// Generate the <option> tags from the optionList:
+		for(Object option : this.optionList){
+			String optionTag = String.format(OPTION_TEMPLATE, option.toString());
+			out.println(optionTag);
+		}
+	} // END of doTag() method
+	
+	// Our implementation used a few String constants to make the code more readable:
+	private  static final String ATTR_TEMPLATE = "%s='%s'"; 
+	private  static final String OPTION_TEMPLATE = "<option value='%1$s'> %1$s </option>";
+	
+} // END of SelectTagHandler
